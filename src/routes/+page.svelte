02f8dc892dc5f7c68319ad799f503dea32173810
ashishkhuraishy <ashishkhuraishy@gmail.com>
2023-03-17 19:31:10 +0530

    feat: implemented css lib + a basic sudoku grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto">
+	<h1>Sudokku</h1>
+	<div class="h-10" />
+
+	{#each Array(9) as _}
+		{#each Array(9) as _}
+			<input type="number" class="w-10 h-10 border-2 border-black" />
+		{/each}
+        <br>
+	{/each}
+</div>
