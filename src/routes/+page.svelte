8c3e30710ea9bc61d103fbdcfb3c3accc41eaf99
ashishkhuraishy <ashishkhuraishy@gmail.com>
2023-03-25 13:57:59 +0530

    feat: basic sudoku grids with input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,60 @@
-<div class="container mx-auto">
-	<h1>Sudokku</h1>
-	<div class="h-10" />
+<script lang="ts">
+	let sudokuGrid = Array(9)
+		.fill(0)
+		.map(() => Array(9).fill(null));
+	let selectedNumber = 1;
 
-	{#each Array(9) as _}
-		{#each Array(9) as _}
-			<input type="number" class="w-10 h-10 border-2 border-black" />
-		{/each}
-        <br>
-	{/each}
+	let row = 0,
+		col = 0;
+
+	function selectNumber(number: number) {
+		selectedNumber = number;
+
+		sudokuGrid[row][col] = selectedNumber;
+	}
+
+	function setInput(row: number, col: number, event: any) {
+		try {
+			let val = parseInt(event?.target?.value ?? 0);
+
+			if (val == 0 || isNaN(val)) {
+				sudokuGrid[row][col] = null;
+				return;
+			}
+
+			sudokuGrid[row][col] = val;
+		} catch (error) {
+			sudokuGrid[row][col] = null;
+		}
+	}
+
+	function setColumn(selectedRow: number, selectedCol: number) {
+		row = selectedRow;
+		col = selectedCol;
+	}
+</script>
+
+<div class="container mx-auto py-10 px-5 font-mono">
+	<div class="text-center">
+		<h1 class="text-4xl mb-5">Sudoku App</h1>
+	</div>
+
+	<div class="flex justify-center">
+		<div class="grid grid-cols-9">
+			{#each sudokuGrid as row, i}
+				{#each row as _, j}
+					<input
+						type="text"
+						inputmode="numeric"
+						pattern="[1-9]"
+						maxlength="1"
+						bind:value={sudokuGrid[i][j]}
+						on:input={(e) => setInput(i, j, e)}
+						on:click={(_) => setColumn(i, j)}
+						class="w-8 h-8 md:w-12 md:h-12 text-center border border-gray-300"
+					/>
+				{/each}
+			{/each}
+		</div>
+	</div>
 </div>
---
+++ src/utils/generator.ts
@@ -0,0 +1,14 @@
+export function generator(): number[][] {
+    const result: number[][] = []
+    for (let i = 0; i < 9; i++) {
+        result[i] = []
+        for (let j = 0; j < 9; j++) {
+            result[i][j] = 0
+        }
+    }
+
+    // sudoku generator
+    
+
+    return result;
+}
