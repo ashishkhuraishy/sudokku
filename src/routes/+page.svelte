20f9ac8848933ca671b80e3389ce0089d281baec
ashishkhuraishy <ashishkhuraishy@gmail.com>
2023-03-25 15:36:26 +0530

    feat: validation support for invalid entries based on rules

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { checkRepeatingNumbers } from '../utils/validate';
+	var invalidGrids: Set<string> = new Set();
+
 	let sudokuGrid = Array(9)
 		.fill(0)
 		.map(() => Array(9).fill(null));
@@ -6,16 +9,16 @@
 	function setInput(row: number, col: number, event: any) {
 		try {
 			let val = parseInt(event?.target?.value ?? 0);
-
 			if (val == 0 || isNaN(val)) {
-				sudokuGrid[row][col] = null;
-				return;
+				throw new Error('Invalid input');
 			}
 
 			sudokuGrid[row][col] = val;
 		} catch (error) {
 			sudokuGrid[row][col] = null;
 		}
+
+		invalidGrids = checkRepeatingNumbers(sudokuGrid);
 	}
 </script>
 
@@ -36,6 +39,7 @@
 						bind:value={sudokuGrid[i][j]}
 						on:input={(e) => setInput(i, j, e)}
 						class="w-8 h-8 md:w-12 md:h-12 text-center border border-gray-300 
+            {invalidGrids.has(`${i},${j}`) ? 'bg-red-500 border-red-700' : ''}
             {j % 3 == 0 ? 'border-l-2 border-l-gray-400' : ''}
             {i % 3 == 0 ? 'border-t-2 border-t-gray-400' : ''}"
 					/>
---
+++ src/utils/validate.ts
@@ -0,0 +1,59 @@
+// check if any number is repeating in a row
+export const checkRepeatingNumbers = (numbers: number[][]) => {
+    const invalidRowColPairs: string[] = [];
+    
+    // finding invalid rows
+    numbers.map((row, index) => {
+        // remove null from row
+        const filteredRow: number[] = row.filter((num) => num !== null);
+        const set = new Set(filteredRow);
+        if(set.size == filteredRow.length){
+            return;
+        }
+        
+        for (let i = 0; i < 9; i++) {
+            invalidRowColPairs.push(`${index},${i}`);
+        }
+
+        return;
+    });
+
+    // find invalid columns
+    for (let index = 0; index < 9; index++) {
+        const column = numbers.map((row) => row[index]);
+        const filteredColumn = column.filter((num) => num !== null);
+        const set = new Set(filteredColumn);
+        if (set.size === filteredColumn.length) {
+            continue;
+        }
+
+        for (let i = 0; i < 9; i++) {
+            invalidRowColPairs.push(`${i},${index}`);
+        }
+    } 
+
+    // find invalid boxes
+    for (let index = 0; index < 9; index++) {
+        const box = [];
+        const rowStart = Math.floor(index / 3) * 3;
+        const colStart = (index % 3) * 3;
+        for (let i = rowStart; i < rowStart + 3; i++) {
+            for (let j = colStart; j < colStart + 3; j++) {
+                box.push(numbers[i][j]);
+            }
+        }
+        const filteredBox = box.filter((num) => num !== null);
+        const set = new Set(filteredBox);
+        if (set.size === filteredBox.length) {
+            continue;
+        }
+
+        for (let i = rowStart; i < rowStart + 3; i++) {
+            for (let j = colStart; j < colStart + 3; j++) {
+                invalidRowColPairs.push(`${i},${j}`);
+            }
+        }
+    }
+
+    return new Set(invalidRowColPairs);
+}
